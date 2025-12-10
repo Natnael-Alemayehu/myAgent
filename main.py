6b07d2e573f6ef1870934d42cb4be0cef56b7edd
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2025-12-10 09:28:44 +0300

    listing files functionality completed.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
-
-
+from google.genai import types
+import functions
 
 
 def main():
@@ -13,8 +13,9 @@ def main():
         epilog="For more help please contact se.natnael.alemayehu@gmail.com"
     )
     parser.add_argument("user_prompt", type=str, help="user prompt to be sent to Gemini AI")
+    parser.add_argument("--verbose", action="store_true", help="prints debug information of the cli" )
     args = parser.parse_args()
-    
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     
@@ -23,18 +24,24 @@ def main():
     client = genai.Client(api_key=api_key)
 
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=args.prompt
+        model='gemini-2.5-flash', contents=messages
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
-        
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}") 
+    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}") 
+
     print("Response:")
     print(response.text)
 
-
+    print(os.path.join("calculator", "."))
+    
 
 if __name__ == "__main__":
     main()
